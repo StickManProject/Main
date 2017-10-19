2f14be96aa95d61f4a0e84ccf7acdb6647b73ea3
Mooppy <32912771+Mooppy@users.noreply.github.com>
2017-10-19 10:34:57 +0200

    Add files via upload

---
+++ ressource/map/Tileset/Logic.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Logic" tilewidth="32" tileheight="32" tilecount="25" columns="5">
+ <image source="../Tiles/logic.png" width="160" height="160"/>
+</tileset>
---
+++ ressource/map/Tileset/White.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="White" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="../Tiles/White.png" width="50" height="46"/>
+</tileset>
